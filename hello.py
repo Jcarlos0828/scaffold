d85b25d9783e570a01cbb7f3d94d02214e172f75
Jose Carlos <jose.carlos4013@gmail.com>
2023-10-14 18:16:43 +0000

    Included add and subs function with test of both funcs

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
-def add(x, y):
-    return x + y
+def toyou(x):
+    return f"hi {x}"
+
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
+    
 
 #var=    
-result = add(1, 2)
-print(f"This is the sum: 1, 2, {result}")
+result = subtract(10)
+print(f"This is the subs: 10, 1, {result}")
